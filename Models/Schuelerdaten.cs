6e6e8891b944dcdabca31f0a3d7671fc5e3224a9
Mario Harrer <mario.harrer438@gmail.com>
2025-03-07 08:12:20 +0100

    Projektdateien hinzufügen.

---
+++ Models/Schuelerdaten.cs
@@ -0,0 +1,9 @@
+﻿namespace SchuelerCheckIN2025.Models
+{
+    public class Schuelerdaten
+    {
+        public int Id { get; set; }
+        public string email { get; set; }
+        public string schluessel {  get; set; }
+    }
+}
